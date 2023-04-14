1970ff1880157eb37c4d68e74e5e34f10c99ad3a
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-04-14 17:43:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+noseX=0;
+noseY=0;
+difference= 0;
+left_wristX=0;
+right_wristX=0;
+
+
+
 function setup() 
 {
     video= createCapture(VIDEO);
     video.size(550,500);
 
 canvas=createCanvas(550,500);
-canvas.position(600,100);
+canvas.position(600,135);
 
 poseNet= ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
@@ -12,7 +20,11 @@ poseNet.on('pose', gotPoses);
 
 function draw ()
 {
-    background('#ba8706');     
+    background('#ba8706'); 
+    fill("#9011fa");
+    stroke("#9011fa");
+    square(noseX, noseY , difference);
+    document.getElementById("square_side").innerHTML="Width & Height of the Square will be =" + difference + "px";
 }
 
 function modelLoaded()
@@ -25,5 +37,14 @@ function gotPoses(results)
 if (results.length > 0)
 {
     console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("Nose x =" + noseX + "And Nose Y = " + noseY);
+
+    left_wristX=results[0].pose.leftWrist.x;
+    right_wristX=results[0].pose.rightWrist.x;
+    console.log("left wrist X =" +  left_wristX + "And right wrist X = " +  right_wristX);
+
+    difference= floor(left_wristX - right_wristX);
 }
 }
